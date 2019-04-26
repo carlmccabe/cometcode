404935144211dc9f71cbca621edaf4087d62e4ea
carlmccabe <carl.h.mccabe@gmail.com>
2019-04-26 15:46:36 +1000

    main.rb with colorize method

--- main.rb
+++ main.rb
@@ -4,27 +4,60 @@ require './question'
 require 'io/console'
 require 'colorized_string'
 
-#Main methods
+#Main methods - main ui
 
 right_name = ""#to confirm username
 def clear
     print "\e[2J\e[f" #to clear screen
 end
 
+def colorString(string, txtcolor = "green", bgcolor= "black")
+    puts ColorizedString["#{string}"].colorize(
+        :color => txtcolor.to_sym,
+        :background => bgcolor.to_sym
+    )
+
+end
+
 while right_name != "y" do
     clear 
     puts "    Welcome to CometCode"
-    puts "--++====================++--"
-    puts ""
-    puts "Please Enter Username:" #asks the username
+    colorString("--++====================++--", "blue")
+    puts
+    puts ColorizedString["Enter Username:"].colorize(
+        :color => :black, 
+        :background => :white
+        )#asks the username
     username = gets.chomp
     puts "Username is <#{username}>? (y)es (n)o"
     right_name = (STDIN.getch.downcase)
     
 end
 clear
-puts "!!!!  Welcome to CometCode #{username}!  !!!!" #prints the username
-puts "____________________________________________+"
+colorString(
+    "Welcome to CometCode #{username}!",
+    "black",
+    "white")
+colorString(
+    "Test your skills as a Ruby Developer!",
+    "black",
+    "white")
+colorString(
+    "Instructions to play:",
+    "red"
+)
+colorString(
+    "-Pick a level\n
+    -Read the questions carefully\n
+    -Select a letter of your answer\n
+    -The result will be displayed at the end of the game\n
+    -Enjoy",
+    "yellow"
+)
+colorString(
+    "____________________________________________+",
+    "yellow"
+    )
 puts "What level would you like to select?       |"
 puts "Easy: 1, Medium: 2, Hard: 3                |"
 puts "(press the 1, 2,or 3 key on the keyboard)  |"
@@ -32,20 +65,20 @@ level = STDIN.getch.to_i
 
 until level.between?(1,3)
 
-    puts "Level has to be between 1 and 3"
-    level = gets.chomp.to_i
+    colorString("Level has to be between 1 and 3", "red")
+    level = STDIN.getch.to_i
 end
 
 case level
 when 1
     questions = YAML.load_file('./easyQuestions.yml')
-    puts "EASY LEVEL"
+    colorString("EASY LEVEL", "blue")
 when 2
     questions = YAML.load_file('./mediumQuestions.yml')
-    puts "MEDIUM LEVEL"
+    colorString("MEDIUM LEVEL", "blue")
 when 3
     questions = YAML.load_file('./hardQuestions.yml')
-    puts "HARD LEVEL"
+    colorString("HARD LEVEL", "blue")
 end
 
 puts "\n" #creates a blank line
@@ -53,8 +86,9 @@ puts "\n" #creates a blank line
 # puts questions
 
 round = Round.new(questions)
-round.print_all 
+# round.print_all 
 round.round_loop
+
 
 
 
